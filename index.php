dc0aaa6ae4dee888f6c40c5df143ea22edb42d95
Revinarahmadilla30 <90024616+Revinarahmadilla30@users.noreply.github.com>
2022-04-05 19:56:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html> <!--Penggunaan tag <!doctype html> untuk menginformasikan bahwa program adalah dokumen html-->
+<html lang="en"> <!--Penggunaan tag <html> untuk tempat semua elemen dokumen html-->
+  <head> <!--Elemen yang berfungsi sebagai tempat untuk metadata atau informasi-informasi halaman web-->
+      <!-- Nama: Revina Hani Rahmadila -->
+      <!-- NIM: 202410101107 -->
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>KPK Buka Pintu untuk Angelina Sondakh Laporkan Dalang Kasus Hambalang</title> <!--penggunaan tag <title> untuk memberikan judul pada dokumen html-->
+  </head>
+  <body> <!--Elemen yang berfungsi sebagai tempat untuk mengisi halaman web yang akan ditampilkan pada halaman tersebut-->
+      <main class="container">
+          <section class="judul text-center"> <!--Isi untuk header atau judul, penulis, dan gambar-->
+              <h1><strong>KPK Buka Pintu untuk Angelina Sondakh Laporkan Dalang Kasus Hambalang</strong></h1>
+              <p>Tempo.co - 2 jam yang lalu</p>
+              <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAVRTxp.img?h=600&w=800&m=6&q=60&o=f&l=f" alt="" srcset="">
+          </section>
+          <section class="my-5"> <!--Isi untuk berita yang akan ditampilkan-->
+              <h4><p><strong>TEMPO.CO, Jakarta</strong> - Komisi Pemberantasan Korupsi membuka pintu kepada mantan terpidana kasus korupsi <strong>Angelina Sondakh</strong> untuk memberikan laporan tentang kasus korupsi Hambalang ke ke lembaganya. KPK menyatakan akan menerima laporan tersebut.</p>
+              <p>“KPK mengajak pihak yang mengetahui dan memiliki bukti awal dugaan korupsi untuk dapat melaporkan aduannya ke KPK,” kata pelaksana tugas juru bicara KPK Ali Fikri, Selasa, 5 April 2022.</p>
+              <p>Ali mengatakan tim KPK akan memvalidasi laporan tersebut. KPK akan menyelidik aduan itu bila memenuhi syarat kasus yang bisa ditangani komisi antirasuah.</p>
+              <p>KPK, kata dia, berharap mantan terpidana korupsi dapat menyampaikan pesan berisi ajakan kepada masyarakat untuk jangan melakukan tindak pidana korupsi. “Karena hukuman pidana korupsi secara nyata memberikan efek jera tidak hanya bagi diri pelaku, tapi juga berdampak kepada keluarga, kerabat, dan lingkungan sekitar,” kata dia.</p>
+              <p>Sebelumnya, Angelina Sondakh dalam salah satu acara televisi mengatakan mengetahui dalang di balik korupsi proyek Wisma Atlet Hambalang. Namun, dia mengatakan tak ingin membongkarnya demi keselamatan anaknya, Keanu Massaid.</p>
+              <p>Angelina mendekam di penjara dalam kasus korupsi wisma atlet Jakabaring yang dibangun untuk SEA Games Jakarta-Palembang. Mantan Politikus Partai Demokrat itu dinilai terbukti bersalah menerima suap Rp 2,5 miliar dan US$ 1,2 juta dari Grup Permai.</p>
+              <p>Angie masuk penjara pada 27 April 2012 setelah Pengadilan Tindak Pidana Korupsi Jakarta menghukumnya 10 tahun penjara dan denda Rp 500 juta subsider 6 bulan kurungan. Putusan itu dikuatkan pada tingkat banding.</p>
+              <p>Pada tingkat kasasi, Mahkamah Agung memperberat putusan itu. Majelis hakim yang dipimpin oleh Artidjo Alkostar menghukum perempuan yang akrab disapa Angie itu menjadi 12 tahun penjara. Dia juga diwajibkan membayar uang pengganti Rp 2,5 miliar dan US$ 1,2 juta. Hukuman tersebut kembali turun menjadi 10 tahun setelah pihak Angelina Sondakh mengajukan peninjauan kembali.</p></h4>
+          </section>
+          <section class="text-center">
+              <h5>Revina Hani Rahmadila - 202410101107</h5>
+          </section>
+      </main>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
